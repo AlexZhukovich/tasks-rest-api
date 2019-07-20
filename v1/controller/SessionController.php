ee368ba9bcfd5f582e45e178b32180c5a4d08505
Alex Zhukovich <zhukovich.a@gmail.com>
2019-07-20 07:58:12 +0200

    Added support for delete session operation; Updated .htaccess file

--- v1/controller/SessionController.php
+++ v1/controller/SessionController.php
@@ -15,7 +15,76 @@
     }
 
     if (array_key_exists("sessionid", $_GET)) {
+        $sessionid = $_GET['sessionid'];
 
+        if ($session_id === '' || !is_numeric($sessionid)) {
+            $response = new Response();
+            $response->setHttpStatusCode(400);
+            $response->setSuccess(false);
+            ($sessionid === '' ? $response->addMessage("Session ID cannot be blank") : false);
+            (!is_numeric(sessionid) ? $response->addMessage("Session ID must be blank") : false);
+            $response->send();
+            exit;
+        }
+
+        if (!isset($_SERVER['HTTP_AUTHORIZATION']) || strlen($_SERVER['HTTP_AUTHORIZATION']) < 1) {
+            $response = new Response();
+            $response->setHttpStatusCode(401);
+            $response->setSuccess(false);
+            (!isset($_SERVER['HTTP_AUTHORIZATION']) ? $response->addMessage("Access token is missing from the header") : false);
+            (strlen($_SERVER['HTTP_AUTHORIZATION']) < 1 ? $response->addMessage("Access Token cannot be blank") : false);
+            $response->send();
+            exit;
+        }
+
+        $accesstoken = $_SERVER['HTTP_AUTHORIZATION'];
+
+        if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+            try{
+                $query = $writeDb->prepare('DELETE FROM tbl_sessions WHERE id = :sessionid AND accesstoken = :accesstoken');
+                $query->bindParam(':sessionid', $sessionid, PDO::PARAM_INT);
+                $query->bindParam(':accesstoken', $accesstoken, PDO::PARAM_STR);
+                $query->execute();
+
+                $rowCount = $query->rowCount();
+                if ($rowCount === 0) {
+                    $response = new Response();
+                    $response->setHttpStatusCode(400);
+                    $response->setSuccess(false);
+                    $response->addMessage("Failed to log out of this session using access token provided");
+                    $response->send();
+                    exit;
+                }
+
+                $returnData = array();
+                $returnData['session_id'] = intval($sessionid);
+                
+                $response = new Response();
+                $response->setHttpStatusCode(200);
+                $response->setSuccess(true);
+                $response->addMessage("Logged out");
+                $response->setData($returnData);
+                $response->send();
+                exit;
+
+            } catch (PDOException $ex) {
+                $response = new Response();
+                $response->setHttpStatusCode(500);
+                $response->setSuccess(false);
+                $response->addMessage("Issue was an logging out - please try again");
+                $response->send();
+                exit;
+            }
+        } elseif ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+
+        } else {
+            $response = new Response();
+            $response->setHttpStatusCode(405);
+            $response->setSuccess(false);
+            $response->addMessage("Request method not allowed");
+            $response->send();
+            exit;
+        }
     } else if (empty($_GET)) {
         if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
             $response = new Response();
